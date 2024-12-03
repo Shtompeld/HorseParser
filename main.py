492ebd400141c9c5a836fc5179ccdb991c062531
Shtompeld <54930491+Shtompeld@users.noreply.github.com>
2024-12-03 21:07:25 +0600

    Done

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import os
+import re
+import json
 
-import requests
-from time import sleep
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class HorseParser:
@@ -18,7 +18,7 @@ class HorseParser:
 
         self.browser.close()
         self.browser.quit()
-
+    #Проверка, существует ли xpath
     def xpath_exists(self, url):
 
         browser = self.browser
@@ -28,50 +28,41 @@ class HorseParser:
         except NoSuchElementException:
             exist = False
         return exist
-
-    def class_name_exists(self, cls):
-
-        browser = self.browser
-        try:
-            browser.find_element(By.CLASS_NAME, cls)
-            exist = True
-        except NoSuchElementException:
-            exist = False
-        return exist
-
+    #Получить название породы
     def get_breed_name(self, url):
-        self.browser.get(url)
-        sleep(1)  # Ждём загрузку страницы
+        if self.browser.current_url != url:
+            self.browser.get(url)
+        WebDriverWait(self.browser, 10).until(ec.presence_of_element_located((By.TAG_NAME, "html")))  # Ждём загрузку страницы
         horse_breed_xpath = "/html/body/div[1]/div[2]/div[1]/h1"
 
         if self.xpath_exists(horse_breed_xpath):
-            horse_breed = self.browser.find_elements(By.XPATH, horse_breed_xpath)
-            horse_breed = [item.text for item in horse_breed]  # Порода
-            return horse_breed[0]
+            return self.browser.find_elements(By.XPATH, horse_breed_xpath)[0].text# Порода
+
         return None
+    #Получить характеристики породы по переданному блоку
+    def get_characteristic(self, parent_element_xpath):
 
-    def get_characteristics(self, core_ref):
-
-        core = f"{core_ref}/dl[1]"
-        dictionary = {}
+        core = f"{parent_element_xpath}/dl[1]"
+        characteristics = {}
         i = 1
         while self.xpath_exists(core):
-            dictionary[
+            characteristics[
                 self.browser.find_element(By.XPATH, f"{core}/dt/span").text
             ] = self.browser.find_element(By.XPATH, f"{core}/dd").text
             i += 1
-            core = f"{core_ref}/dl[{i}]"
-        return dictionary
+            core = f"{parent_element_xpath}/dl[{i}]"
+        return characteristics
+    #Получить ссылки на все посты(породы)
+    def get_urls_all_breeds(self, url):
 
-    def get_urls_all_horses(self,url):
+        if self.browser.current_url != url:
+            self.browser.get(url)
 
-        self.browser.get(url)
-        #sleep(1)
-        main_xpath="/html/body/div/div[2]/div[2]/div[5]"
+        main_xpath = "/html/body/div/div[2]/div[2]/div[5]"
 
         if self.xpath_exists(main_xpath):
             path_group = f"{main_xpath}/div[1]"
-            breed_list=[]
+            breed_list = []
             j = 1
             while self.xpath_exists(path_group):
                 i = 1
@@ -86,105 +77,158 @@ class HorseParser:
                 path_group = f"{main_xpath}/div[{j}]"
 
             return breed_list
-
-    def get_breed_info(self, url):
-
-        horse_breed = self.get_breed_name(url).replace("Лошадь ", "")
-        print(horse_breed)
+    #Получить характеристики породы
+    def get_breed_characteristics(self, url):
 
         if self.browser.current_url != url:
             self.browser.get(url)
+
+        characteristics = {}
 
         # Переходим на вкладку с характеристиками
         if self.xpath_exists("/html/body/div[1]/div[2]/div[2]/ul/li[2]/a"):
             self.browser.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[2]/ul/li[2]/a").click()
 
-        main_features_dict = self.get_characteristics("/html/body/div[1]/div[2]/div[2]/div/div[2]/div[1]")
-        print(main_features_dict)
+        main_features_dict = self.get_characteristic("/html/body/div[1]/div[2]/div[2]/div/div[2]/div[1]")
+        characteristics["main_features"] = main_features_dict
 
-        appearance_dict = self.get_characteristics("/html/body/div[1]/div[2]/div[2]/div/div[2]/div[2]")
-        print(appearance_dict)
+        appearance_dict = self.get_characteristic("/html/body/div[1]/div[2]/div[2]/div/div[2]/div[2]")
+        characteristics["appearance"] = appearance_dict
 
-        character_temperament_dict = self.get_characteristics("/html/body/div[1]/div[2]/div[2]/div/div[2]/div[3]")
-        print(character_temperament_dict)
+        character_temperament_dict = self.get_characteristic("/html/body/div[1]/div[2]/div[2]/div/div[2]/div[3]")
+        characteristics["character_temperament"] = character_temperament_dict
 
-        care_maintenance_dict = self.get_characteristics("/html/body/div[1]/div[2]/div[2]/div/div[2]/div[4]")
-        print(care_maintenance_dict)
+        care_maintenance_dict = self.get_characteristic("/html/body/div[1]/div[2]/div[2]/div/div[2]/div[4]")
+        characteristics["care_maintenance_dict"] = care_maintenance_dict
 
+        return characteristics
+    #Получить последний абзац
     @staticmethod
     def get_last_paragraph(parent_element_xpath, parent_element):
         last_paragraph = parent_element.find_elements(By.XPATH,
                                                       f"{parent_element_xpath}//following::div[@class='text' and not(following::h2)]/*")
         par = ""
         for paragraph in last_paragraph:
+            #Убираем часть с видео
             if not paragraph.text.__contains__("видео"):
                 par += f"{paragraph.text}\n"
         return par
-
+    #Получить абзацы между последним и первым
     @staticmethod
     def get_middle_paragraph(parent_element, xpath):
 
         el = parent_element.find_elements(By.XPATH,xpath)
         par = ""
-
         for subparagraph in el:
             par += f"{subparagraph.text}\n"
-
         return par
-
-    def get_article(self, browser, url):
-
-        if browser.current_url != url:
-            browser.get(url)
+    #Получить изображения
+    @staticmethod
+    def get_images(parent_element_xpath, parent_element,attrib):
+            images = parent_element.find_elements(By.XPATH,f"{parent_element_xpath}")
+            return (img.get_attribute(attrib) for img in images)
+    #Получить статью
+    def get_article(self, url):
+            if self.browser.current_url != url:
+                self.browser.get(url)
 
             # На сайте 2 варианта страницы, с боковой панелью и без
             if self.xpath_exists("/html/body/div[1]/div[2]/div[2]/div/div[1]/article/aside/div"):
-                parent_element_xpath= "/html/body/div[1]/div[2]/div[2]/div/div[1]/article/div[4]"
+                parent_element_xpath = "/html/body/div[1]/div[2]/div[2]/div/div[1]/article/div[4]"
             else:
-                parent_element_xpath="/html/body/div[1]/div[2]/div[2]/div/div[1]/article"
+                parent_element_xpath ="/html/body/div[1]/div[2]/div[2]/div/div[1]/article"
 
-            parent_element = browser.find_element(By.XPATH, parent_element_xpath)
+            parent_element = self.browser.find_element(By.XPATH, parent_element_xpath)
 
             article = {}
-            h2_list=parent_element.find_elements(By.XPATH,f"{parent_element_xpath}/h2")
+            h2_list = parent_element.find_elements(By.XPATH,f"{parent_element_xpath}/h2")#Получить все заголовки
+
+            #Записываем первый параграф
             if self.xpath_exists("/html/body/div[1]/div[2]/div[2]/div/div[1]/article/aside/div"):
-                article["first_paragraph"]=parent_element.find_element(By.XPATH,f"{parent_element_xpath}/div[1]/p").text
+                article["first_paragraph"] = parent_element.find_element(By.XPATH,f"{parent_element_xpath}/div[1]/p").text
             else:
                 article["first_paragraph"] = parent_element.find_element(By.XPATH,f"{parent_element_xpath}/div[4]/p").text
 
+            #Записываем параграфы между первым и последним
+            for i in range(0, len(h2_list)):
+                article[f"header{i+1}"] = h2_list[i].text#Добавить заголовок
+                if i < len(h2_list) - 1:
+                    article[f"paragraph{i+2}"] = self.get_middle_paragraph(parent_element,
+                    f"{parent_element_xpath}//following::div[@class='text' and (preceding::h2[@id='{h2_list[i].get_attribute("id")}'])"
+                    f" and (following::h2[@id='{h2_list[i + 1].get_attribute("id")}'])]/*") #Добавить параграф
 
-            for i in range(0, len(h2_list) - 1):
-                article[f"paragraph{i+2}"] = self.get_middle_paragraph(parent_element,
-                f"{parent_element_xpath}//following::div[@class='text' and (preceding::h2[@id='{h2_list[i].get_attribute("id")}'])"
-                f" and (following::h2[@id='{h2_list[i + 1].get_attribute("id")}'])]/*")
-
+            # Записываем последний параграф
             article["last_paragraph"] = self.get_last_paragraph(parent_element_xpath, parent_element)
 
+            imgs_from_article = []
+            domain=url.split("/")[2] #Получаем домен сайта
 
-            print(article)
+            #Получаем динамические изображения
+            #Адрес разбит по тэгам домен + s + r + c
+            imgs_from_article_s = list(self.get_images(f"{parent_element_xpath}//following::img[@s]",parent_element,"s"))
+            imgs_from_article_r = list(self.get_images(f"{parent_element_xpath}//following::img[@s]",parent_element,"r"))
+            imgs_from_article_c = list(self.get_images(f"{parent_element_xpath}//following::img[@s]",parent_element,"c"))
 
+            #Получаем изображения из статьи
+            for i in range(0,len(imgs_from_article_s)):
+                imgs_from_article.append(f"https://{domain}{imgs_from_article_s[i]}{imgs_from_article_r[i]}{imgs_from_article_c[i]}")
+                #Заменяем миниатюры на оригинальные изображения
+                imgs_from_article[i] = (imgs_from_article[i].replace(
+                    re.search(r"(\w+/(\d*))-\d*/",imgs_from_article[i]).group(), "/orig/"))
 
+            #Записываем изображения из статьи
+            article["images_from_article"] = imgs_from_article
 
-    def test(self, url):
-        # try:
-           #urls=self.get_urls_all_horses(browser, "https://stroy-podskazka.ru/loshadi/porody/")
+            return article
+    #Получить породу
+    def get_breed(self, url):
+        if self.browser.current_url != url:
+            self.browser.get(url)
+        WebDriverWait(self.browser, 10).until(ec.presence_of_element_located((By.TAG_NAME, "html")))
+        # Записываем название(удаляя из него лишнее) и характеристики
+        breed = {"copyright": url,
+                 "name": self.get_breed_name(url).replace("Лошадь ", ""),
+                 "characteristics": self.get_breed_characteristics(url)}
 
-           self.get_article(self.browser, url)
+        general_images=[]
+        #Получаем основные изображения
+        if self.xpath_exists("/html/body/div[1]/div[2]/div[2]/div/div[1]/article/div[1]/div[1]/div[2]/div/img"):
+            general_images = self.get_images("/html/body/div[1]/div[2]/div[2]/div/div[1]/article/div[1]/div[1]/div[2]/div/img",
+                                     self.browser, "src")
+            # Заменяем миниатюры на оригинальные изображения
+            general_images = list((img.replace(
+                re.search(r"(\w+/(\d*))-\d*/", img).group(), "/orig/") for img in general_images))
 
-           # for u in range(11, 12):
-           #     self.get_breed_info(browser, urls[u])
-           #     print("\n-----------------------------------")
+        #Записываем основные изображения
+        breed["general_images"] = general_images
+        #Получаем статью
+        article = self.get_article(url)
+        #Записываем статью
+        breed["article"] = article
 
+        return breed
+    #Собрать все данные в json
+    def create_breeds_json(self):
+        urls=self.get_urls_all_breeds("https://stroy-podskazka.ru/loshadi/porody/")
 
+        for u in range(0, len(urls)):
+            print(f"{u+1}/{len(urls)} - {self.get_breed_name(urls[u])}")
 
-        # except  Exception as ex:
-        #     print(ex)
+            breed = self.get_breed(urls[u])
+
+            with open(f"outputs/{breed["name"]}.json", "w", encoding="utf-8") as f:
+                json.dump(breed, f, indent=4, ensure_ascii=False)
+
+            #if u%10==0 and u!=0:
+              #  sleep(80)#Вроде бы сайт начинает тупить от кол-ва запросов
 
 hp = HorseParser()
 
 #hp.test("https://stroy-podskazka.ru/loshadi/porody-i-masti/altajskogo-kraya/")
-hp.test("https://stroy-podskazka.ru/loshadi/porody/bulonskaya/")
-
-sleep(10000)
+hp.create_breeds_json()
 
 hp.close_browser()
+
+# with open('result.json', 'r', encoding="utf-8") as json_file:
+#     data = json.load(json_file)
+#     print(data)
